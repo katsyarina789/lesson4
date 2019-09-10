2adef4ba0057024b4b24df5f5317dad7ef38e29d
katsyarina789 <katsyarina.fedorova97@gmail.com>
2019-09-10 17:44:21 +0300

    lesson5

--- src/lesson5/ht1.java
+++ src/lesson5/ht1.java
@@ -5,11 +5,15 @@ import java.util.ArrayList;
 public class ht1 {
     public static void main(String[] args) {
         int sum = 0;
-        ArrayList<Integer[]> list = new ArrayList<>();
-       for (int i = 0; i<list.size(); i++) {
-
-
-       }
-
-         }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = -50; i < 50; i++) {
+            list.add(i);
+        }
+        double average = 0;
+        for (int j = 0; j < 5; j++) {
+            int x = (int) (Math.random() * list.size());
+            average = (x / 5.0);
+        }
+        System.out.println(average);
+    }
 }
