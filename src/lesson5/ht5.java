e3584f9eae95d2a7d07052727f85aed3089d65c3
katsyarina789 <katsyarina.fedorova97@gmail.com>
2019-09-10 17:30:58 +0300

    lesson5

---
+++ src/lesson5/ht5.java
@@ -0,0 +1,19 @@
+package lesson5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ht5{
+    public static void main(String[] args) {
+        String string = "Вывести на консоль, сколько раз каждый символ встречается в строке";
+        String str = null;
+        char[] Array1 = str.toCharArray();
+
+        Map<Integer, String> letters = new HashMap<Integer, String>();
+
+        for (int i = 0; i < Array1.length; i++);
+
+        System.out.println(letters);
+    }
+
+    }
